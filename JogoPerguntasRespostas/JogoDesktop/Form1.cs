0782e7f275013b229fd9ef9d5e5cda0a04e9ad90
Guto Xavier <gutoffline@gmail.com>
2017-01-18 17:33:22 -0200

    Criação da tela inicial

---
+++ JogoPerguntasRespostas/JogoDesktop/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDesktop
+{
+    public partial class frmInicial : Form
+    {
+        public frmInicial()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNome_Enter(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+        }
+
+        private void txtNome_Leave(object sender, EventArgs e)
+        {
+
+            if(txtNome.Text == "")
+            {
+                txtNome.Text = "Digite seu nome";
+            }
+
+        }
+    }
+}
