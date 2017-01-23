d90e888397aa00017abc0f1a9bb8ae44ed3f077f
Guto Xavier <gutoffline@gmail.com>
2017-01-23 17:52:22 -0200

    Banco e Protótipo

--- JogoPerguntasRespostas/JogoDesktop/Pergunta1.Designer.cs
+++ JogoPerguntasRespostas/JogoDesktop/Pergunta1.Designer.cs
@@ -91,7 +91,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(211, 293);
+            this.button1.Location = new System.Drawing.Point(149, 271);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 45);
             this.button1.TabIndex = 5;
